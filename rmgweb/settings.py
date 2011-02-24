750f1662e3f54c9062ef0aceb932f6192b983e06
Josh Allen <jwallen@mit.edu>
2011-02-24 18:55:48 -0500

    Some initial configuration of the Django project.
    * Added settings.PROJECT_PATH to make the full path to the Django
      project available.
    
    * Added myself as an admin.
    
    * Set the database to use SQLite (at least for now).
    
    * Set the time zone to Eastern time.
    
    * Set up the media and template paths.
    
    * Enabled the admin interface, since we expect to use it.
    
    * Enabled serving of static media by Django in development mode only. DO
      NOT use in a production environment; use a web server (e.g. Apache)
      instead!

--- rmgweb/settings.py
+++ rmgweb/settings.py
@@ -1,9 +1,17 @@
-# Django settings for rmgweb project.
+"""
+The Django settings for the RMG website.
+"""
+
+import os.path
+
+# The full path of the Django project (as determined from the location of this file)
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
+    ('Josh Allen', 'jwallen@mit.edu')
     # ('Your Name', 'your_email@domain.com'),
 )
 
@@ -11,8 +19,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',                 # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_PATH, 'rmgweb.db'),        # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -27,7 +35,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -43,19 +51,19 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_PATH,'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'i+a0dzd@buc8k!4#(zn!*h#f4iinp*_v@6cu!08u_3^uwbzwp#'
@@ -81,6 +89,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -90,7 +99,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- rmgweb/urls.py
+++ rmgweb/urls.py
@@ -1,16 +1,29 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+import os
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^rmgweb/', include('rmgweb.foo.urls')),
+    # (r'^website/', include('website.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
+
+# When developing in Django we generally don't have a web server available to
+# serve static media; this code enables serving of static media by Django
+# DO NOT USE in a production environment!
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(.*)$', 'django.views.static.serve',
+            {'document_root': os.path.join(settings.PROJECT_PATH, 'media'),
+             'show_indexes': True, }
+        ),
+    )
