f5eb2bfabc110d1b7fc8d90a7984d0f583d33a8a
Josh Allen <jwallen@mit.edu>
2011-02-26 21:40:04 -0500

    Added first version of thermo table template.
    This template is used to provide a summary of the entries in a given
    component (subsection) of the thermo database. Still plenty of work to
    do in deciding what attributes to show for each entry, and how to
    arrange things visually.

--- rmgweb/database/views.py
+++ rmgweb/database/views.py
@@ -2,8 +2,10 @@ import os.path
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import Http404
 import settings
 
+from rmgpy.chem.thermo import *
 from rmgpy.data.thermo import ThermoDatabase
 
 ################################################################################
@@ -23,4 +25,49 @@ def thermo(request, section='', subsection=''):
     """
     The RMG database homepage.
     """
-    return render_to_response('thermo.html', {'section': section, 'subsection': subsection, 'thermoDatabase': thermoDatabase}, context_instance=RequestContext(request))
+    # Make sure section has an allowed value
+    if section not in ['depository', 'libraries', 'groups', '']:
+        raise Http404
+
+    if subsection != '':
+
+        # A subsection was specified, so render a table of the entries in
+        # that part of the database
+        
+        # Determine which subsection we wish to view
+        if section == 'depository':
+            try:
+                database = thermoDatabase.depository[subsection]
+            except KeyError:
+                raise Http404
+        elif section == 'libraries':
+            libraries = [library for library in thermoDatabase.libraries if library.label == subsection]
+            if len(libraries) != 1: raise Http404
+            database = libraries[0]
+        elif section == 'groups':
+            try:
+                database = thermoDatabase.groups[subsection]
+            except KeyError:
+                raise Http404
+
+        # Sort entries by index
+        entries0 = database.library.values()
+        entries0.sort(key=lambda entry: entry.index)
+
+        entries = []
+        for entry in entries0:
+
+            structure = '<pre>%s</pre>' % entry.item.toAdjacencyList(removeH=True)
+
+            if isinstance(entry.data, ThermoGAModel): dataFormat = 'Group additivity'
+            elif isinstance(entry.data, WilhoitModel): dataFormat = 'Wilhoit'
+            elif isinstance(entry.data, NASAModel): dataFormat = 'NASA'
+
+            entries.append((entry.index,entry.label,structure,dataFormat))
+
+        return render_to_response('thermoTable.html', {'section': section, 'subsection': subsection, 'databaseName': database.name, 'entries': entries}, context_instance=RequestContext(request))
+
+    else:
+        # No subsection was specified, so render an outline of the thermo
+        # database components
+        return render_to_response('thermo.html', {'section': section, 'subsection': subsection, 'thermoDatabase': thermoDatabase}, context_instance=RequestContext(request))
