3d22531d32721d28d2445b95212ff40484f68e02
Richard West <r.west@neu.edu>
2012-07-03 20:37:44 -0400

    Use cached template loader, and monitor the templates.
    Haven't tested how much this speeds things up, but it can't help,
    especially with all the little bits of JSmath being template files.
    
    Also added an example wsgi file, to show how to monitor the templates for changes.
    I tried to make the wsgi file pretty generic (i.e. no absolute paths to our webserver)

--- rmgweb/settings.py
+++ rmgweb/settings.py
@@ -83,9 +83,11 @@ ADMIN_MEDIA_PREFIX = '/admin/media/'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
+('django.template.loaders.cached.Loader', (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
+)),
 )
 
 MIDDLEWARE_CLASSES = (
