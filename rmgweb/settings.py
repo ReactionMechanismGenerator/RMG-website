f132b18ee90b2ea2ed62adb91481cfeedcfb1809
Nick Vandewiele <nickvandewiele@gmail.com>
2015-10-07 17:42:25 -0400

    removed deprecated django.contrib.markup from settings

--- rmgweb/settings.py
+++ rmgweb/settings.py
@@ -120,7 +120,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.markup',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
