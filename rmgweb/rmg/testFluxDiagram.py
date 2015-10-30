2f110a3415a8db8beeb5f2494615c8cc3f332a86
Nick Vandewiele <nickvandewiele@gmail.com>
2015-10-30 10:13:32 -0400

    create another unit test for flux diagrams

--- rmgweb/rmg/testFluxDiagram.py
+++ rmgweb/rmg/testFluxDiagram.py
@@ -6,11 +6,16 @@ from rmgpy.tools.fluxdiagram import *
 
 class GenerateFluxDiagramTest(unittest.TestCase):
 
-    def test_minimal(self):
+    def test_minimal_use_species_drawings(self):
+    	"""
+    	Uses pre-generated png species drawings to include
+    	in the video.
+    	"""
     	folder = os.path.join(os.getcwd(),'rmgweb/rmg/tests/minimal')
         inputFile = os.path.join(folder,'input.py')
+        speciesPath = os.path.join(os.path.dirname(inputFile), 'species_pngs')
 
-        run(inputFile)
+        run(inputFile, speciesPath)
 
         outputdir = os.path.join(folder, 'flux/')
         outputdir_sys1 = os.path.join(outputdir, '1/')
@@ -18,3 +23,23 @@ class GenerateFluxDiagramTest(unittest.TestCase):
         self.assertTrue(os.path.isfile(outputfile))
         
         shutil.rmtree(outputdir)
+
+    def test_minimal_do_not_use_species_drawings(self):
+    	"""
+    	Generate a video with species labels, without species drawings.
+    	"""
+    	folder = os.path.join(os.getcwd(),'rmgweb/rmg/tests/minimal')
+        inputFile = os.path.join(folder,'input.py')
+
+        run(inputFile)
+
+        outputdir = os.path.join(folder, 'flux/')
+        
+        outputdir_sys1 = os.path.join(outputdir, '1/')
+        outputfile = os.path.join(outputdir_sys1, 'flux_diagram.avi')
+        self.assertTrue(os.path.isfile(outputfile))
+        
+        shutil.rmtree(outputdir)
+
+        speciesdir = os.path.join(folder, 'species/')
+        shutil.rmtree(speciesdir)
