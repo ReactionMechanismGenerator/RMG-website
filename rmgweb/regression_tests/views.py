a6aeeade89386486d558125fac300e4426021a03
KEHANG <kehanghan@gmail.com>
2017-03-21 23:21:35 -0400

    once creating job RMG-website will spawn a testing job

--- rmgweb/regression_tests/views.py
+++ rmgweb/regression_tests/views.py
@@ -21,6 +21,23 @@ def add_job(request):
 		rmgpy_branch = request.POST['autocompleteField_py']
 		rmgdb_branch = request.POST['autocompleteField_db']
 		job = RegressionTestJob.objects.create(rmgpy_branch=rmgpy_branch, rmgdb_branch=rmgdb_branch)
+		spawn_test_job(rmgpy_branch, rmgdb_branch)
 		return redirect('/regression_tests')
 	else:
 		return redirect('/regression_tests')
+
+
+def spawn_test_job(rmgpy_branch, rmgdb_branch):
+	
+	import subprocess
+	import os
+
+	jobs = 'eg1'
+	rmg_tests_script = os.path.join(os.environ["RMGTESTS"], 'local_tests', 'exe.sh')
+	command = ['bash',
+	           rmg_tests_script,
+	           rmgpy_branch,
+	           rmgdb_branch,
+	           jobs]                         
+    
+	subprocess.Popen(command)
