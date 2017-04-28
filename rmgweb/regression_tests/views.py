402281e69d868ff670f95d078ec150d5be5c2b2c
KEHANG <kehanghan@gmail.com>
2017-04-28 11:30:07 -0400

    adding crash checking

--- rmgweb/regression_tests/views.py
+++ rmgweb/regression_tests/views.py
@@ -39,41 +39,54 @@ def spawn_test_job(rmgpy_branch, rmgdb_branch,job, request):
     import os
     import time
     import threading
+    import random
 
 
-    command = 'echo spawning job > testing.txt'
+    open('testing.txt','w').close()
+    write = open('testing.txt','w')
 
+
+    if(random.random() > .25):
+        write.write('Testing finished')
+    time.sleep(12)
 
     threading.Timer(TIME_TO_SEARCH, file_exists,args=(rmgpy_branch, rmgdb_branch, job, request)).start()
 
-    os.system(command)
-    #time.sleep(12
 	# import subprocess
     # import os
     # command = 'Spawning job'
-	# jobs = rmgpy_branch+'_'+rmgdb_branch
-	# rmg_tests_script = os.path.join(os.environ["RMGTESTS"], 'local_tests', 'exe.sh')
-	# command = ['bash',
-	#            rmg_tests_script,
-	#            rmgpy_branch,
-	#            rmgdb_branch,
-	#            jobs]
-
-	#subprocess.Popen(command)
+    # jobs = rmgpy_branch+'_'+rmgdb_branch
+    # rmg_tests_script = os.path.join(os.environ["RMGTESTS"], 'local_tests', 'exe.sh')
+    # command = ['bash',
+    # rmg_tests_script,
+    # rmgpy_branch,
+    # rmgdb_branch,
+    # # jobs]
+    #
+    # subprocess.Popen(command)
 
 def file_exists(rmgpy, rmgdb, job, request):
-
     import os
     import threading
-    command = "[ -f testing.txt ] && echo 'Found' || echo 'Not found'"
-    is_found = os.popen(command).read().strip()
+    import subprocess
+
+    command = "[ -f testing.txt ] && echo 'Found' || echo 'Not found"
+    is_found = os.system(command)
     test = rmgpy+'--'+rmgdb
 
-    os.system('echo '+is_found)
-    if(is_found == 'Found'):
-        os.system('echo "The file has been found"')
-        job.job_status = '/static/img/icons/done.jpg'
-        job.save()
+    os.system('echo '+str(is_found))
+    if(is_found > 0):
+        command = "grep 'Testing finished' testing.txt"
+        status = os.system(command)
+        os.system('echo '+str(status))
+        if(status < 256):
+            os.system('echo "The file has been found"')
+            job.job_status = '/static/img/icons/done.jpg'
+            job.save()
+        else:
+            os.system('echo "The file crashed"')
+            job.job_status = '/static/img/icons/redX.png'
+            job.save()
 
     else:
         os.system('echo "The file is not yet done"')
