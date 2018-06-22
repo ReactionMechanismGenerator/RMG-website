71a7d1928b1c8e663074e406476f20979b8cff2f
Xiaorui <xiaorui@mit.edu>
2018-06-22 15:42:55 -0400

    Updated the urls

--- rmgweb/urls.py
+++ rmgweb/urls.py
@@ -80,10 +80,15 @@ urlpatterns = [
     url(r'^user/(?P<username>\w+)$', rmgweb.main.views.viewProfile),
 
     # Password reset
-    url(r'^password_reset/$',auth_view.password_reset,{'template_name': 'password_reset_form.html'}, name = 'password_reset'),
-    url(r'^password_reset/done/$',auth_view.password_reset_done, name = 'password_reset_done'),
-    url(r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<tAoken>[0-9A-Za-z]{1,13}-[0-9-Za-z]{1,20})/$',auth_view.password_reset_confirm, name = 'password_reset_confirm'),
-    url(r'^password_reset/complete/',auth_view.password_reset_complete, name = 'password_reset_complete'),
+    url(r'^password_reset/$',auth_view.password_reset,{'template_name': 'password_reset_form.html',
+       'email_template_name':'password_reset_email.html', 'subject_template_name':'password_reset_subject.txt'},
+        name = 'password_reset'),
+    url(r'^password_reset/done/$',auth_view.password_reset_done, {'template_name': 'password_reset_done.html'},
+        name = 'password_reset_done'),
+    url(r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9-Za-z]{1,20})/$',
+        auth_view.password_reset_confirm, {'template_name': 'password_reset_confirm.html'}, name = 'password_reset_confirm'),
+    url(r'^password_reset/complete/',auth_view.password_reset_complete, {'template_name': 'password_reset_complete.html'},
+        name = 'password_reset_complete'),
 
     # Database
     url(r'^database/', include('rmgweb.database.urls')),
